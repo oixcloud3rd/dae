78da2d5cab24c4b0372c6ef9510f10667c84fb9b
Nagase Mintao <80738880+NagaseMinato@users.noreply.github.com>
2026-07-25 17:08:02 +0800

    fix(package): preserve inactive service on upgrade

--- install/package_after_install.sh
+++ install/package_after_install.sh
@@ -3,7 +3,7 @@
 if [ $(command -v systemctl) ]; then
 	systemctl daemon-reload
 
-	if [ $(systemctl is-active dae) ]; then
+	if systemctl is-active --quiet dae.service; then
 		systemctl restart dae.service
 		echo "Restarting dae service, it might take a while."
 	fi
